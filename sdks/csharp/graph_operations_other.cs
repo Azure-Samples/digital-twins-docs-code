1e7edc738105192a5709c5dda5ad77868e7cdc6c
David R. Williamson <drwill@microsoft.com>
2021-01-29 12:02:05 -0800

    More samples code cleanup

--- sdks/csharp/graph_operations_other.cs
+++ sdks/csharp/graph_operations_other.cs
@@ -1,20 +1,22 @@
 // ------------------ CREATE RELATIONSHIP (Short) ---------------------
 // <CreateRelationship_short>
-BasicRelationship rel = new BasicRelationship();
-rel.TargetId = "myTargetTwin";
-rel.Name = "contains"; // a relationship with this name must be defined in the model
+var rel = new BasicRelationship
+{
+    TargetId = "myTargetTwin",
+    Name = "contains", // a relationship with this name must be defined in the model
+};
 // Initialize properties
-Dictionary<string, object> props = new Dictionary<string, object>();
+var props = new Dictionary<string, object>();
 props.Add("active", true);
 rel.Properties = props;
-client.CreateOrReplaceRelationshipAsync("mySourceTwin", "rel001", rel);
+await client.CreateOrReplaceRelationshipAsync("mySourceTwin", "rel001", rel);
 // </CreateRelationship_short>
 
 // ------------------ CREATE RELATIONSHIP (Alternate) ---------------------
 // <CreateRelationship_3>
 // Create Twins, using functions similar to the previous sample
-await CreateRoom("Cafe", 70, 66);
-await CreateFloor("GroundFloor", averageTemperature=70);
+await CreateRoomAsync("Cafe", 70, 66);
+await CreateFloorAsync("GroundFloor", averageTemperature = 70);
 // Create relationships
 var relationship = new BasicRelationship
 {
@@ -25,7 +27,8 @@ try
 {
     string relId = $"GroundFloor-contains-Cafe";
     await client.CreateOrReplaceRelationshipAsync<BasicRelationship>("GroundFloor", relId, relationship);
-} catch(ErrorResponseException e)
+}
+catch (ErrorResponseException e)
 {
     Console.WriteLine($"*** Error creating relationship: {e.Response.StatusCode}");
 }
@@ -33,11 +36,13 @@ try
 
 // ------------------ LIST PROPERTIES OF RELATIONSHIPS ---------------------
 // <ListRelationshipProperties>
-BasicRelationship res = client.GetRelationship<BasicRelationship>(twin_id, rel_id);
+var res = await client.GetRelationshipAsync<BasicRelationship>(twin_id, rel_id);
 Console.WriteLine($"Relationship Name: {rel.Name}");
 foreach (string prop in rel.Contents.Keys)
 {
     if (twin.Contents.TryGetValue(prop, out object value))
+    {
         Console.WriteLine($"Property '{prop}': {value}");
+    }
 }
 // </ListRelationshipProperties>
