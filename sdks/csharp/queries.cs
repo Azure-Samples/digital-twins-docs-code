406c3a5720e43f3f6da5fbf81488672078ed107e
Bailey Anderson <baanders@microsoft.com>
2021-07-16 15:17:12 -0400

    Remove unused samples

--- sdks/csharp/queries.cs
+++ sdks/csharp/queries.cs
@@ -27,33 +27,3 @@ catch (RequestFailedException ex)
     throw;
 }
 // </FullQuerySample>
-
-// ------------------ Paging pattern for query API ---------------------
-// <PagedQuery>
-try
-{
-    // Repeat the query while there are pages
-    string query = "SELECT * FROM digitaltwins";
-    AsyncPageable<BasicDigitalTwin> queryResult = await client.QueryAsync(query);
-    int page = 0;
-    await foreach (Page<BasicDigitalTwin> page in queryResult.AsPages())
-    {
-        Console.WriteLine($"== Query results page {++page}:");
-
-        // Extract the query-charge header from the page
-        if (QueryChargeHelper.TryGetQueryCharge(page, out float queryCharge))
-        {
-            Console.WriteLine($"Query charge was: {queryCharge}");
-        }
-
-        foreach (BasicDigitalTwin twin in page.Values)
-        {
-            Console.WriteLine($"  Found {twin.Id}");
-        }
-    }
-}
-catch (RequestFailedException ex)
-{
-    Console.WriteLine($"*** Error in twin query: {ex.Status}, {ex.ErrorCode}, {ex.Message}");
-}
-// </PagedQuery>
